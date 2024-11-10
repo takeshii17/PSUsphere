b8eaf8c822120e68ac4606f7a3ed861ba3abcb5a
takeshii17 <20228299@psu.palawan.edu.ph>
2024-11-11 05:17:48 +0800

    wah

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
+    
+    # Updated URL pattern to avoid overlap
     path('organization_list/', OrganizationList.as_view(), name='organization-list'),
     path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),
-    path('organization_list/<pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
